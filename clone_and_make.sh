5f200519f80ecaafbf7901a01a698ab533fd9510
Christian Buhtz <c.buhtz@posteo.jp>
2024-05-27 21:34:45 +0200

    Related to #1548

--- clone_and_make.sh
+++ clone_and_make.sh
@@ -2,6 +2,7 @@
 # 1. The current BIT repo will be cloned into a temporary directory.
 # 2. The debian folder and helper scripts are copied into that folder.
 # In the end "makedeb.sh" can be executed to build the deb file.
+# See Issue 1548 in "bit-team/backintime" repo for more details.
 BIT_GIT_URL='https://github.com/bit-team/backintime.git'
 
 # Create temp folder
